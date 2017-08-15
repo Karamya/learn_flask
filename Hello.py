7bc38be102ffe4291eb310eecf6143652325d273
karamya <karthickphysics@gmail.com>
2017-08-15 16:10:42 +0200

    int and float options on the url links

--- Hello.py
+++ Hello.py
@@ -1,17 +1,19 @@
 # @Author: karthick
 # @Date:   2017-08-15T14:54:29+02:00
 # @Last modified by:   karthick
-# @Last modified time: 2017-08-15T15:30:21+02:00
+# @Last modified time: 2017-08-15T16:08:40+02:00
 
 from flask import Flask
 app = Flask(__name__)
 
 #route() function is a decorator that tells the application which URL should call the associated function
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello %s! :)" % name
+@app.route('/blog/<int: postID>')
+def show_blog(postID):
+    return "Blog Number %d! :)" % postID
 
-
+@app.route('/blog/<float: revNo>')
+def revision(revNo):
+    return "Revision Number %f! :)" % revNo
 
 if __name__=="__main__":
-    app.run('0.0.0.0', 5000,debug=True) # when debug is enabled, the server will reload itself if the code changes
+    app.run(debug=True) # when debug is enabled, the server will reload itself if the code changes
