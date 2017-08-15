f294da96d235b4f7e4e658fd1e70b45cd7d16b83
karamya <karthickphysics@gmail.com>
2017-08-15 17:21:24 +0200

    added http methods post and Get

--- Hello.py
+++ Hello.py
@@ -1,42 +1,24 @@
 # @Author: karthick
 # @Date:   2017-08-15T14:54:29+02:00
 # @Last modified by:   karthick
-# @Last modified time: 2017-08-15T16:39:29+02:00
+# @Last modified time: 2017-08-15T17:05:28+02:00
 
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
-#route() function is a decorator that tells the application which URL should call the associated function
-@app.route('/blog/<int:postID>')
-def show_blog(postID):
-    return "Blog Number %d! :)" % postID
+@app.route('/success/<name>')
+def success(name):
+    return "welcome %s" % name
 
-@app.route('/rev/<float:revNo>')
-def revision(revNo):
-    return "Revision Number %0.3f! :)" % revNo
-
-@app.route('/flask')
-def hello_flask():
-   return 'Hello Flask'
-
-@app.route('/python/')
-def hello_python():
-   return 'Hello Python'
-
-@app.route('/admin')
-def hello_admin():
-    return 'Hello Admin'
-
-@app.route('/guest/<guest>')
-def hello_guest(guest):
-    return 'Hello %s as Guest' % guest
-
-@app.route('/user/<name>')
-def hello_user(name):
-    if name =='admin':
-        return redirect(url_for('hello_admin'))
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['nm']
+        return redirect(url_for('success', name=user))
     else:
-        return redirect(url_for('hello_guest', guest=name))
+        user =request.args.get('nm')
+        return redirect(url_for('success', name=user))
+
 
 if __name__=="__main__":
     app.run(debug=True) # when debug is enabled, the server will reload itself if the code changes
